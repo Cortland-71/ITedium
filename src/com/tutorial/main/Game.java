959155c0aa10c59b0aa625ee7648e6a9c83b0218
Cortland Carrillo <cortland.carrillo.dev@gmail.com>
2019-11-24 18:40:30 -0700

    First commit

---
+++ src/com/tutorial/main/Game.java
@@ -0,0 +1,84 @@
+package com.tutorial.main;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.nio.Buffer;
+
+public class Game extends Canvas implements Runnable{
+
+    public static final int WIDTH = 640, HEIGHT = WIDTH/12 * 9;
+    private Thread thread;
+    private boolean running = false;
+    private Handler handler;
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, "GAME", this);
+        handler = new Handler();
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = 1_000_000_000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running)
+                render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
+        handler.tick();
+    }
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.GREEN);
+        g.fillRect(0,0, WIDTH, HEIGHT);
+        handler.render(g);
+        g.dispose();
+        bs.show();
+    }
+
+    public static void main(String[] args) {
+        new Game().start();
+    }
+}
---
+++ src/com/tutorial/main/Handler.java
@@ -0,0 +1,28 @@
+package com.tutorial.main;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+public class Handler {
+
+    private LinkedList<GameObject> object = new LinkedList<>();
+
+    public void tick() {
+        for (GameObject obj : object) {
+            obj.tick();
+        }
+    }
+    public void render(Graphics g) {
+        for (GameObject obj : object) {
+            obj.render(g);
+        }
+    }
+
+    public void addObject(GameObject object) {
+        this.object.add(object);
+    }
+
+    public void removeObject(GameObject object) {
+        this.object.remove(object);
+    }
+}
